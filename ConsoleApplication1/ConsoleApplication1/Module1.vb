fbaabc4b86b72d0b0e5ee5f45dc4151bdc44320f
ito takumi <itotakumi8@gmail.com>
2018-12-15 13:42:25 +0900

    カレンダー作成

---
+++ ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim firstDayOfMonth As Date = New Date(2018, 4, 1)
+        RenderCalender(firstDayOfMonth.DayOfWeek, Date.DaysInMonth(firstDayOfMonth.Year, firstDayOfMonth.Month))
+    End Sub
+
+    Sub RenderCalender(firstDay As Integer, dayLength As Integer)
+
+        Console.WriteLine("日 月 火 水 木 金 土")
+        For i = 1 To firstDay
+            Console.Write("   ")
+        Next
+
+        For i = 1 To dayLength
+            If i < 10 Then
+                Console.Write(" ")
+            End If
+            Console.Write(i & " ")
+
+            If (i + firstDay) Mod 7 = 0 Then
+                Console.WriteLine()
+            End If
+        Next
+        Console.WriteLine()
+    End Sub
+End Module
