abce0747850b777d91e91519f187895a693dc8b0
ito takumi <itotakumi8@gmail.com>
2018-12-19 13:44:15 +0900

    電卓作成

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,298 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox = New System.Windows.Forms.TextBox()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.ButtonClear = New System.Windows.Forms.Button()
+        Me.ButtonEqual = New System.Windows.Forms.Button()
+        Me.ButtonPlus = New System.Windows.Forms.Button()
+        Me.ButtonMinus = New System.Windows.Forms.Button()
+        Me.ButtonX = New System.Windows.Forms.Button()
+        Me.ButtonDivide = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox
+        '
+        Me.TextBox.Font = New System.Drawing.Font("MS UI Gothic", 40.0!)
+        Me.TextBox.ForeColor = System.Drawing.Color.Black
+        Me.TextBox.Location = New System.Drawing.Point(7, 8)
+        Me.TextBox.Multiline = True
+        Me.TextBox.Name = "TextBox"
+        Me.TextBox.Size = New System.Drawing.Size(298, 61)
+        Me.TextBox.TabIndex = 0
+        Me.TextBox.Text = "0"
+        Me.TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.TextBox.UseWaitCursor = True
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.White
+        Me.Button7.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button7.ForeColor = System.Drawing.Color.MediumAquamarine
+        Me.Button7.Location = New System.Drawing.Point(7, 74)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(70, 70)
+        Me.Button7.TabIndex = 1
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.White
+        Me.Button8.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button8.ForeColor = System.Drawing.Color.MediumAquamarine
+        Me.Button8.Location = New System.Drawing.Point(83, 74)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(70, 70)
+        Me.Button8.TabIndex = 2
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.White
+        Me.Button9.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button9.ForeColor = System.Drawing.Color.MediumAquamarine
+        Me.Button9.Location = New System.Drawing.Point(159, 74)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(70, 70)
+        Me.Button9.TabIndex = 3
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.White
+        Me.Button4.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.MediumAquamarine
+        Me.Button4.Location = New System.Drawing.Point(7, 150)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(70, 70)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.White
+        Me.Button5.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button5.ForeColor = System.Drawing.Color.MediumAquamarine
+        Me.Button5.Location = New System.Drawing.Point(83, 150)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(70, 70)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.White
+        Me.Button6.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button6.ForeColor = System.Drawing.Color.MediumAquamarine
+        Me.Button6.Location = New System.Drawing.Point(159, 150)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(70, 70)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.MediumAquamarine
+        Me.Button1.Location = New System.Drawing.Point(7, 226)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(70, 70)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.MediumAquamarine
+        Me.Button2.Location = New System.Drawing.Point(83, 226)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(70, 70)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.White
+        Me.Button3.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.MediumAquamarine
+        Me.Button3.Location = New System.Drawing.Point(159, 226)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(70, 70)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button0
+        '
+        Me.Button0.BackColor = System.Drawing.Color.White
+        Me.Button0.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button0.ForeColor = System.Drawing.Color.MediumAquamarine
+        Me.Button0.Location = New System.Drawing.Point(7, 302)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(70, 70)
+        Me.Button0.TabIndex = 10
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = False
+        '
+        'ButtonClear
+        '
+        Me.ButtonClear.BackColor = System.Drawing.Color.MediumSeaGreen
+        Me.ButtonClear.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ButtonClear.ForeColor = System.Drawing.Color.White
+        Me.ButtonClear.Location = New System.Drawing.Point(83, 302)
+        Me.ButtonClear.Name = "ButtonClear"
+        Me.ButtonClear.Size = New System.Drawing.Size(70, 70)
+        Me.ButtonClear.TabIndex = 11
+        Me.ButtonClear.Text = "C"
+        Me.ButtonClear.UseVisualStyleBackColor = False
+        '
+        'ButtonEqual
+        '
+        Me.ButtonEqual.BackColor = System.Drawing.Color.MediumAquamarine
+        Me.ButtonEqual.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ButtonEqual.ForeColor = System.Drawing.Color.White
+        Me.ButtonEqual.Location = New System.Drawing.Point(159, 302)
+        Me.ButtonEqual.Name = "ButtonEqual"
+        Me.ButtonEqual.Size = New System.Drawing.Size(70, 70)
+        Me.ButtonEqual.TabIndex = 12
+        Me.ButtonEqual.Text = "="
+        Me.ButtonEqual.UseVisualStyleBackColor = False
+        '
+        'ButtonPlus
+        '
+        Me.ButtonPlus.BackColor = System.Drawing.Color.MediumAquamarine
+        Me.ButtonPlus.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ButtonPlus.ForeColor = System.Drawing.Color.White
+        Me.ButtonPlus.Location = New System.Drawing.Point(235, 74)
+        Me.ButtonPlus.Name = "ButtonPlus"
+        Me.ButtonPlus.Size = New System.Drawing.Size(70, 70)
+        Me.ButtonPlus.TabIndex = 13
+        Me.ButtonPlus.Text = "＋"
+        Me.ButtonPlus.UseVisualStyleBackColor = False
+        '
+        'ButtonMinus
+        '
+        Me.ButtonMinus.BackColor = System.Drawing.Color.MediumAquamarine
+        Me.ButtonMinus.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ButtonMinus.ForeColor = System.Drawing.Color.White
+        Me.ButtonMinus.Location = New System.Drawing.Point(235, 150)
+        Me.ButtonMinus.Name = "ButtonMinus"
+        Me.ButtonMinus.Size = New System.Drawing.Size(70, 70)
+        Me.ButtonMinus.TabIndex = 14
+        Me.ButtonMinus.Text = "－"
+        Me.ButtonMinus.UseVisualStyleBackColor = False
+        '
+        'ButtonX
+        '
+        Me.ButtonX.BackColor = System.Drawing.Color.MediumAquamarine
+        Me.ButtonX.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ButtonX.ForeColor = System.Drawing.Color.White
+        Me.ButtonX.Location = New System.Drawing.Point(235, 226)
+        Me.ButtonX.Name = "ButtonX"
+        Me.ButtonX.Size = New System.Drawing.Size(70, 70)
+        Me.ButtonX.TabIndex = 15
+        Me.ButtonX.Text = "×"
+        Me.ButtonX.UseVisualStyleBackColor = False
+        '
+        'ButtonDivide
+        '
+        Me.ButtonDivide.BackColor = System.Drawing.Color.MediumAquamarine
+        Me.ButtonDivide.Font = New System.Drawing.Font("MS UI Gothic", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ButtonDivide.ForeColor = System.Drawing.Color.White
+        Me.ButtonDivide.Location = New System.Drawing.Point(235, 302)
+        Me.ButtonDivide.Name = "ButtonDivide"
+        Me.ButtonDivide.Size = New System.Drawing.Size(70, 70)
+        Me.ButtonDivide.TabIndex = 16
+        Me.ButtonDivide.Text = "÷"
+        Me.ButtonDivide.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.Color.MediumAquamarine
+        Me.ClientSize = New System.Drawing.Size(312, 378)
+        Me.Controls.Add(Me.ButtonDivide)
+        Me.Controls.Add(Me.ButtonX)
+        Me.Controls.Add(Me.ButtonMinus)
+        Me.Controls.Add(Me.ButtonPlus)
+        Me.Controls.Add(Me.ButtonEqual)
+        Me.Controls.Add(Me.ButtonClear)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.TextBox)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "電卓"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button0 As System.Windows.Forms.Button
+    Friend WithEvents ButtonClear As System.Windows.Forms.Button
+    Friend WithEvents ButtonEqual As System.Windows.Forms.Button
+    Friend WithEvents ButtonPlus As System.Windows.Forms.Button
+    Friend WithEvents ButtonMinus As System.Windows.Forms.Button
+    Friend WithEvents ButtonX As System.Windows.Forms.Button
+    Friend WithEvents ButtonDivide As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,152 @@
+﻿Public Class Form1
+
+    Private ope As String = Nothing     '今回押されたボタンの数字以外の値を保存(+,-,x,÷,=,C)
+    Private beforeIn As String = "C"    '今回押されたボタンの値を保存(1,2,3,4,5,6,7,8,9,0,+,-,x,÷,=,C)
+    Private ans As Long = 0             '演算結果を保存
+    Private num As Long = 0             '今回入力された数値を保存する変数
+
+    '1ボタンクリック処理
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If beforeIn >= "0" And beforeIn <= "9" Then
+            ' 直前に押されたのが数字ボタンのとき
+            ' かつ、8桁未満の数値が入力されているとき
+            If TextBox.Text.Length < 9 Then
+                TextBox.Text &= "1"
+            End If
+        Else
+            ' 直前に押されたのが数字以外のボタンのとき
+            TextBox.Text = "1"
+        End If
+        beforeIn = "1"
+    End Sub
+    '2ボタンクリック処理
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        If beforeIn >= "0" And beforeIn <= "9" Then
+            If TextBox.Text.Length < 9 Then
+                TextBox.Text &= "2"
+            End If
+        Else
+            TextBox.Text = "2"
+        End If
+        beforeIn = "2"
+    End Sub
+    '3ボタンクリック処理
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        If beforeIn >= "0" And beforeIn <= "9" Then
+            If TextBox.Text.Length < 9 Then
+                TextBox.Text &= "3"
+            End If
+        Else
+            TextBox.Text = "3"
+        End If
+        beforeIn = "3"
+    End Sub
+    '4ボタンクリック処理
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        If beforeIn >= "0" And beforeIn <= "9" Then
+            If TextBox.Text.Length < 9 Then
+                TextBox.Text &= "4"
+            End If
+        Else
+            TextBox.Text = "4"
+        End If
+        beforeIn = "4"
+    End Sub
+    '5ボタンクリック処理
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        If beforeIn >= "0" And beforeIn <= "9" Then
+            If TextBox.Text.Length < 9 Then
+                TextBox.Text &= "5"
+            End If
+        Else
+            TextBox.Text = "5"
+        End If
+        beforeIn = "5"
+    End Sub
+    '6ボタンクリック処理
+    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Button6.Click
+        If beforeIn >= "0" And beforeIn <= "9" Then
+            If TextBox.Text.Length < 9 Then
+                TextBox.Text &= "6"
+            End If
+        Else
+            TextBox.Text = "6"
+        End If
+        beforeIn = "6"
+    End Sub
+    '7ボタンクリック処理
+    Private Sub Button7_Click(sender As System.Object, e As System.EventArgs) Handles Button7.Click
+        If beforeIn >= "0" And beforeIn <= "9" Then
+            If TextBox.Text.Length < 9 Then
+                TextBox.Text &= "7"
+            End If
+        Else
+            TextBox.Text = "7"
+        End If
+        beforeIn = "7"
+    End Sub
+    '8ボタンクリック処理
+    Private Sub Button8_Click(sender As System.Object, e As System.EventArgs) Handles Button8.Click
+        If beforeIn >= "0" And beforeIn <= "9" Then
+            If TextBox.Text.Length < 9 Then
+                TextBox.Text &= "8"
+            End If
+        Else
+            TextBox.Text = "8"
+        End If
+        beforeIn = "8"
+    End Sub
+    '9ボタンクリック処理
+    Private Sub Button9_Click(sender As System.Object, e As System.EventArgs) Handles Button9.Click
+        If beforeIn >= "0" And beforeIn <= "9" Then
+            If TextBox.Text.Length < 9 Then
+                TextBox.Text &= "9"
+            End If
+        Else
+            TextBox.Text = "9"
+        End If
+        beforeIn = "9"
+    End Sub
+    '0ボタンクリック処理
+    Private Sub Button0_Click(sender As System.Object, e As System.EventArgs) Handles Button0.Click
+        If beforeIn >= "0" And beforeIn <= "9" Then
+            If TextBox.Text.Length < 9 Then
+                TextBox.Text &= "0"
+            End If
+        Else
+            TextBox.Text = "0"
+        End If
+        beforeIn = "0"
+    End Sub
+    '＋ボタンクリック処理
+    Private Sub ButtonPlus_Click(sender As System.Object, e As System.EventArgs) Handles ButtonPlus.Click
+        If beforeIn >= "0" And beforeIn <= "9" Then
+            If ope = "C" Then
+                ' 最初の入力数値をansに保管
+                ans = CInt(TextBox.Text)
+            Else
+                ' ansに保管されている
+                ' ＋ボタンクリック前の数値と加算
+                num = CInt(TextBox.Text)
+                ans = ans + num
+                TextBox.Text = ans
+            End If
+        Else
+            ' 前回クリックが数字以外の時
+            Select Case ope
+                Case "C"
+                    ' 前回がクリアボタン
+                    ans = CInt(TextBox.Text)
+                Case "="
+                    ' 前回がイコールボタン
+                    ans = ans + num
+                Case "+"
+                    num = ans
+            End Select
+        End If
+        beforeIn = "+"
+        ope = "+"
+    End Sub
+End Class
+
+
