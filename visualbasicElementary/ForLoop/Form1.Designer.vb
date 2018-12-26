4d4e780d6e0b8839def8a86f5a24a32e5eeaf085
ito takumi <itotakumi8@gmail.com>
2018-12-26 16:23:20 +0900

    第7回　繰り返し処理

---
+++ visualbasicElementary/ForLoop/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1024, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(71, 31)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "LoopTest"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(1024, 39)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(71, 31)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "DebugTest"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(1024, 76)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(71, 31)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "ループ制御"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(1026, 112)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(69, 19)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(1024, 137)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(71, 31)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "∞"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1097, 500)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+
+End Class
---
+++ visualbasicElementary/ForLoop/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+    'フォームぴったりの円を描く
+    Private Sub Form1_Click(sender As Object, e As System.EventArgs) Handles Me.Click
+        Dim g As Graphics = Me.CreateGraphics
+        Dim tate As Integer
+        Dim yoko As Integer
+        tate = Me.ClientSize.Height - 1
+        yoko = Me.ClientSize.Width - 1
+        For i = 0 To 9900 Step 10
+            g.DrawEllipse(New Pen(Color.FromArgb(i Mod 200, i Mod 160, i Mod 123)), 0 + i, 0 + i, yoko - (i * 2), tate - (i * 2))
+        Next i
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        Me.BackColor = Color.Black
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As System.Windows.Forms.PaintEventArgs) Handles Me.Paint
+        'Dim circleGreen As Graphics = Me.CreateGraphics
+        'circleGreen.DrawEllipse(Pens.Green, 0, 0, 100, 100)
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        'Do
+        '    MsgBox("hello!") '無限ループ
+        'Loop
+        For i = 1 To 5
+            MsgBox("今" & i & "回目")
+        Next
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim B As Integer
+        B = 1
+        'Do Until B > 100
+        '    B = B + B
+        '    Debug.WriteLine(B)
+        Do While B <= 100
+            B = B + B
+            Debug.WriteLine(B)
+        Loop
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Do
+            If TextBox1.Text.Length > 15 Then
+                Exit Do
+            End If
+            TextBox1.AppendText("ぺ")
+        Loop
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        Do
+            MsgBox("hello!") '無限ループ
+        Loop
+    End Sub
+End Class
