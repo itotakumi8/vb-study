5e631db72a6eb3d853e3128ac648490e5989d322
itotakumi8 <itotakumi8@gmail.com>
2018-12-27 01:16:27 +0900

    1回コミット

--- visualbasicElementary/Class/Form1.vb
+++ visualbasicElementary/Class/Form1.vb
@@ -1,10 +1,10 @@
 ﻿Imports System.IO
 Public Class Form1
     '目に見えるクラス＝コントロール
-
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim fileInfoUse As New FileInfo("C:\Users\takumi\Documents\古いドキュメント\eula.txt")
         Me.BackColor = System.Drawing.Color.Red
         MsgBox(Math.PI)
-        MsgBox(FileInfo.Length)
+        MsgBox(fileInfoUse.Length())
     End Sub
 End Class
