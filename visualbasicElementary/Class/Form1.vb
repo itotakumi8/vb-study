7a9bdf4d59b86b802c18fd8eb77c784fc2acb1cb
ito takumi <itotakumi8@gmail.com>
2018-12-26 18:32:52 +0900

    1回コミット

---
+++ visualbasicElementary/Class/Form1.vb
@@ -0,0 +1,10 @@
+﻿Imports System.IO
+Public Class Form1
+    '目に見えるクラス＝コントロール
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Me.BackColor = System.Drawing.Color.Red
+        MsgBox(Math.PI)
+        MsgBox(FileInfo.Length)
+    End Sub
+End Class
