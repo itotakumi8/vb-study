26d4cca67971f81eae1c2da8b674b4654153705c
ito takumi <itotakumi8@gmail.com>
2018-12-27 14:04:37 +0900

    第11回　メソッドを作る

---
+++ visualbasicElementary/makeMathod/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BtnLeft = New System.Windows.Forms.Button()
+        Me.BtnRight = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(188, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(93, 30)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "message"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'BtnLeft
+        '
+        Me.BtnLeft.Location = New System.Drawing.Point(2, 3)
+        Me.BtnLeft.Name = "BtnLeft"
+        Me.BtnLeft.Size = New System.Drawing.Size(44, 30)
+        Me.BtnLeft.TabIndex = 2
+        Me.BtnLeft.Text = "<"
+        Me.BtnLeft.UseVisualStyleBackColor = True
+        '
+        'BtnRight
+        '
+        Me.BtnRight.Location = New System.Drawing.Point(52, 3)
+        Me.BtnRight.Name = "BtnRight"
+        Me.BtnRight.Size = New System.Drawing.Size(44, 30)
+        Me.BtnRight.TabIndex = 3
+        Me.BtnRight.Text = ">"
+        Me.BtnRight.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.BtnRight)
+        Me.Controls.Add(Me.BtnLeft)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents BtnLeft As System.Windows.Forms.Button
+    Friend WithEvents BtnRight As System.Windows.Forms.Button
+
+End Class
---
+++ visualbasicElementary/makeMathod/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+    'メソッドを呼び出すためのイベント
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        'WriteLog("E:\Test.text", "自作メソッドのテスト")
+        ShowTime()
+        GetJapanYear()
+        IsEven(5)
+    End Sub
+    '作成したメソッド本文
+    Private Sub WriteLog(Filename As String, Description As String) 'ファイルに書き込みを行う自作メソッド
+        Dim Write As New IO.StreamWriter(Filename, False)           'インスタンスを作成
+        Write.WriteLine(Now.ToString("yy/MM/dd hh:mm:ss") & " " & Description) '非共有メンバのメソッド・WriteLine を用いて書き込みを行う
+        Write.Close()                                               '終了する
+    End Sub
+    'テスト時刻表示
+    Private Sub ShowTime()
+        MsgBox(Now)
+    End Sub
+    '西暦を和暦で返す値を返すメソッド
+    Private Function GetJapanYear() As Integer
+        Dim Japan As New Globalization.JapaneseCalendar
+        Dim JapaneseYear As Integer
+        JapaneseYear = Japan.GetYear("2005/1/1")
+        MsgBox("平成" & JapaneseYear & "年")
+        Return JapaneseYear
+    End Function
+    '偶数奇数を返すメソッド（値を返す引数ありメソッド）
+    Private Function IsEven(number As Integer) As Boolean
+        If number Mod 2 = 0 Then
+            Return MsgBox("偶数")
+        Else
+            Return MsgBox("奇数")
+        End If
+    End Function
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Black
+    End Sub
+    'まとめ
+    Dim XPoint As Integer = 120 '初期位置
+    Dim YPoint As Integer = 100
+    Private Sub BtnLeft_Click(sender As System.Object, e As System.EventArgs) Handles BtnLeft.Click
+        XPoint -= 10
+        'YPoint += 10
+        DrawCircle(XPoint, YPoint)
+    End Sub
+
+    Private Sub BtnRight_Click(sender As System.Object, e As System.EventArgs) Handles BtnRight.Click
+        XPoint += 10
+        'YPoint -= 10
+        DrawCircle(XPoint, YPoint)
+    End Sub
+    Private Sub DrawCircle(X As Integer, Y As Integer)
+        Dim Drawing As Graphics = Me.CreateGraphics
+        Dim Pattern As New Drawing2D.LinearGradientBrush(New Point(X, Y), New Point(X + 50, Y + 50), Color.Blue, Color.Yellow)
+        Drawing.Clear(Color.Black)
+        Drawing.FillEllipse(Pattern, X, Y, 50, 50)
+    End Sub
+End Class
