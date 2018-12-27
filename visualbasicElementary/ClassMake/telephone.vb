d3021e649d27793cf7189d7834f2ca55f43aa5bd
ito takumi <itotakumi8@gmail.com>
2018-12-27 18:55:43 +0900

    コミット

---
+++ visualbasicElementary/ClassMake/telephone.vb
@@ -0,0 +1,9 @@
+﻿Public Class telephone
+    Private Declare Ansi Function PlaySound Lib "winmm.dll" Alias "PlaySoundA" (IpszName As String, hModule As Integer, dwFlags As Integer)
+    Private Const SAD_ASYNC = &H1
+    Public Sub Bell()
+        Dim WaveFile As Integer
+        WaveFile = Environment.GetEnvironmentVariable("WINDIR") & "\media\ringin.wav"
+        PlaySound(WaveFile, 0, SAD_ASYNC)
+    End Sub
+End Class
