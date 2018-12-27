9bcf6f1b8eabcd8539dbe70026500d106c05fe1f
ito takumi <itotakumi8@gmail.com>
2018-12-27 15:47:33 +0900

    第13回　取りこぼした重要なトピック

---
+++ visualbasicElementary/other/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Const COMPANY_NAME As String = "by Visual Basic中学校" '定数の宣言
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        '使用期間の警告
+        MsgBox("このソフトは試用期間中です", , COMPANY_NAME)
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        'バージョン情報
+        MsgBox("バージョンXXX.XX", , COMPANY_NAME)
+        Me.Close()
+    End Sub
+End Class
