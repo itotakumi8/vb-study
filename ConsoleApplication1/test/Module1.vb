be531a3400a1aeac9430337319e792adf46c0b9f
ito takumi <itotakumi8@gmail.com>
2018-12-28 09:38:41 +0900

    コミット

---
+++ ConsoleApplication1/test/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Do
+            Dim day, zone As Integer
+            Dim bopen As Boolean = True
+
+            Console.WriteLine("曜日を選択してください")
+            Console.Write("0=日曜、1=月曜、2=火曜、3=水曜、4=木曜、5=金曜、6=土曜")
+            day = Integer.Parse(Console.ReadLine())
+
+            Console.WriteLine("時間帯を選択してください")
+            Console.Write("0=午前、1=午後、2=夜間")
+            zone = Integer.Parse(Console.ReadLine())
+
+            If day = 0 Then
+                bopen = False
+            ElseIf zone = 0 And (day = 2 Or day = 5) Then
+                bopen = False
+            ElseIf zone = 1 And day = 6 Then
+                bopen = False
+            ElseIf zone = 2 And (day = 3 Or day = 6) Then
+                bopen = False
+            End If
+
+            If bopen Then
+                Console.WriteLine("診療しています。")
+            Else
+                Console.WriteLine("休診です。")
+            End If
+            Console.ReadLine()
+        Loop
+    End Sub
+
+End Module
---
+++ visualbasicElementary/ClassEvent/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。 
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します。
+
+<Assembly: AssemblyTitle("ClassEvent")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ClassEvent")> 
+<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+<Assembly: Guid("4ec3e065-6348-4d31-be6c-3962b7df9cf0")> 
+
+' アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
