45561c36e3d98b82f490fc4d21cb6af949c72ac4
itotakumi8 <itotakumi8@gmail.com>
2019-01-04 01:38:26 +0900

    コミット

--- Console2/Console10/Module1.vb
+++ Console2/Console10/Module1.vb
@@ -3,6 +3,10 @@
     Sub Main()
         Console.WriteLine("正の整数を入力してください")
         Dim inputText As String = Console.ReadLine()
+        Console.WriteLine("入力値:" + inputText)
+
         Dim inputByte As Byte() = System.Text.Encoding.UTF8.GetBytes(inputText)
+        Dim inputNumber As Byte = 0.0
+
     End Sub
 End Module
